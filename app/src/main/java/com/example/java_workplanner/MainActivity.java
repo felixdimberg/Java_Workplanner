a17afb5141c675dac972f267be0094dc43e0a227
Felix Dimberg <felix.dimberg@live.com>
2019-11-19 18:08:51 +0100

    added add + settings slides, thinking of adding a slide right functionality for backwards.
    Signed-off-by: Felix Dimberg <felix.dimberg@live.com>

--- app/src/main/java/com/example/java_workplanner/MainActivity.java
+++ app/src/main/java/com/example/java_workplanner/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.java_workplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchAddActivity(View view) {
+        Intent intent = new Intent(this, AddActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchSettingsActivity(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
