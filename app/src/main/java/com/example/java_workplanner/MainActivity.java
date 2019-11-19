027937ce1075eff3b91bf67cd7e852095138a788
Emilia <e-milia@live.se>
2019-11-19 22:22:07 +0100

    Added current date
    Signed-off-by: Emilia <e-milia@live.se>

--- app/src/main/java/com/example/java_workplanner/MainActivity.java
+++ app/src/main/java/com/example/java_workplanner/MainActivity.java
@@ -7,6 +7,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.text.DateFormat;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance().format(calendar.getTime());
+
+        TextView textViewDate = findViewById(R.id.text_view_date);
+        textViewDate.setText(currentDate);
     }
 
     public void launchAddActivity(View view) {
@@ -28,5 +40,17 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent (this, SettingsActivity.class);
         startActivity(intent);
     }
+
+    public void upcomingDays(View view) {
+
+
+    }
+
+    public void pastDays(View view) {
+
+    }
+
+
+
 }
 
